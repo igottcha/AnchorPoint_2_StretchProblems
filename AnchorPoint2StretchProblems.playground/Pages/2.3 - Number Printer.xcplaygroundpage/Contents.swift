e8cd6d6ea94bf65346a2b99b7c70fe64b168acd2
Chris Gottfredson <chrisgottfredson@gmail.com>
2020-03-04 11:10:12 -0700

    solved print number for single digit

--- AnchorPoint2StretchProblems.playground/Pages/2.3 - Number Printer.xcplaygroundpage/Contents.swift
+++ AnchorPoint2StretchProblems.playground/Pages/2.3 - Number Printer.xcplaygroundpage/Contents.swift
@@ -35,9 +35,52 @@
 import Foundation
 
 
+let threeDash: String = " ---- "
+let leftBar: String = "|"
+let rightBar: String = "     |"
+let doubleBar: String = "|    |"
 
+let zero = "\(threeDash)\n\(doubleBar)\n\(doubleBar)\n\(doubleBar)\n\(threeDash)"
+let one = "  \(leftBar)\n  \(leftBar)\n  \(leftBar)\n  \(leftBar)\n  \(leftBar)"
+let two = "\(threeDash)\n\(rightBar)\n\(threeDash)\n\(leftBar)\n\(threeDash)"
+let three = "\(threeDash)\n\(rightBar)\n\(threeDash)\n\(rightBar)\n\(threeDash)"
+let four = "\(doubleBar)\n\(doubleBar)\n\(threeDash)\n\(rightBar)\n\(rightBar)"
+let five = "\(threeDash)\n\(leftBar)\n\(threeDash)\n\(rightBar)\n\(threeDash)"
+let six = "\(threeDash)\n\(leftBar)\n\(threeDash)\n\(doubleBar)\n\(threeDash)"
+let seven = "\(threeDash)\n\(doubleBar)\n\(rightBar)\n\(rightBar)\n\(rightBar)"
+let eight = "\(threeDash)\n\(doubleBar)\n\(threeDash)\n\(doubleBar)\n\(threeDash)"
+let nine = "\(threeDash)\n\(doubleBar)\n\(threeDash)\n\(rightBar)\n\(rightBar)"
 
+let numberArray = [zero, one, two, three, four, five, six, seven, eight, nine]
 
+func printNumber(number: Int) {
+    
+    switch number {
+    case 0:
+        print(zero)
+    case 1:
+        print(one)
+    case 2:
+        print(two)
+    case 3:
+        print(three)
+    case 4:
+        print(four)
+    case 5:
+        print(five)
+    case 6:
+        print(six)
+    case 7:
+        print(seven)
+    case 8:
+        print(eight)
+    case 9:
+        print(nine)
+    default:
+        print("I only print single digits")
+    }
+}
 
+printNumber(number: 9)
 
 //: [Ternary Operator](@next)
